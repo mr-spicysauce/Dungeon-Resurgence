8e14546bd2cafb33447eee9b1190042c156cd421
mr_spicysauce <mr_spicysauce@yahoo.com>
2023-05-28 11:03:38 +0100

    📦 NEW: Boot splash screen

--- scripts/lvl_1.gd
+++ scripts/lvl_1.gd
@@ -10,13 +10,12 @@ var random_spawn_ammount
 
 func _ready():
 	GVar.bad_guys_killed_in_lvl = 0
-	
 	random_spawn_ammount = randf_range(4,10)
 	random_spawn_ammount = (random_spawn_ammount * GVar.difficulty)
 	print("spawing " + str(random_spawn_ammount))
 	spawn_bad_guy(random_spawn_ammount)
-	
 	GVar.is_ingame = true
+
 
 func _physics_process(delta):
 	await get_tree().create_timer(1).timeout
